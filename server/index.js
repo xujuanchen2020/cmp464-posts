c52dd590fd623b9cff522287abeba375afd3c723
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-11 13:22:12 -0500

    sever connected to database

--- server/index.js
+++ server/index.js
@@ -7,3 +7,12 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
+
+// https://cloud.mongodb.com/
+const MONGODB_URL = 'mongodb://Posts:123@posts-shard-00-00.kvi1t.mongodb.net:27017,posts-shard-00-01.kvi1t.mongodb.net:27017,posts-shard-00-02.kvi1t.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-d4trke-shard-0&authSource=admin&retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGODB_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>app.listen(PORT, ()=>console.log(`running on port: ${PORT}`)))
+.catch((error)=>console.log(error));
+mongoose.set('useFindAndModify',false); // don't get any warnings at consol
