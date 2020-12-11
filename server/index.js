e3180457c2a5aeda506013f6f525267582ec2210
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-11 13:42:44 -0500

    add controller

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import router from './routes/posts.js';
+import getPosts from './routes/posts.js';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use('/posts', router);
+app.use('/posts', getPosts);
 
 // https://cloud.mongodb.com/
 const MONGODB_URL = 'mongodb://Posts:123@posts-shard-00-00.kvi1t.mongodb.net:27017,posts-shard-00-01.kvi1t.mongodb.net:27017,posts-shard-00-02.kvi1t.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-d4trke-shard-0&authSource=admin&retryWrites=true&w=majority';
