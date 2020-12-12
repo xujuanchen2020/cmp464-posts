63ad17ad4f7719edc7d055cccb7d8f0fd164ba73
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-12 13:51:00 -0500

    add Form, Posts, Post components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import cubic from './images/cubic.png'
+import Form from './Form/Form'
+import Posts from './Posts/Posts'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant='h2' align='center'>
+        <img src={cubic} alt='logo' height='60' width='60' align='center'/>
+        Posts
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
---
+++ client/src/Posts/Post/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div>
+            <h2>post</h2>
+        </div>
+    )
+}
+
+export default Post
